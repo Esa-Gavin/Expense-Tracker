1d198f297cbec9682224b4da1b88dd35c72d1bc1
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-10 18:28:40 +0300

    learn how to interpolate or display data dynamically

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 9);
+    const expenseTitle = 'Car Insurance';
+    const expensePrice = 800;
+
     return (
         <div className='expense-item'>
-            <div>February 9th 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$800</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expensePrice}</div>
             </div>
         </div>
     );
