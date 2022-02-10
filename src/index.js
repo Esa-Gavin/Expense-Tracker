06b4d4bc04becdae8aedb0a8b0e78e7d84c7c36f
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-10 17:04:55 +0300

    create your first component named expenseitem

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ExpenseItem from './components/ExpenseItem';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ExpenseItem />
   </React.StrictMode>,
   document.getElementById('root')
 );
