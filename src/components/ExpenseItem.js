5e6717f1d58d148a2bcb44f4200012b030a52d8d
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-12 17:08:25 +0300

    display the dates dynamically by defining the vars outside the return value

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) { // the word props can be set to any other name //
+    const mwezi = props.tarehe.toLocaleString('en-US', { month: 'long' });
+    const mwaka = props.tarehe.toLocaleString('en-US', { day: '2-digit' });
+    const date = props.tarehe.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>{props.tarehe.toDateString()}</div>
+            <div>
+                <div>{mwezi}</div>
+                <div>{mwaka}</div>
+                <div>{date}</div>
+
+            </div>
+            
             <div className='expense-item__description'>
                 <h2>{props.kichwa}</h2>
                 <div className='expense-item__price'>${props.bei}</div>
