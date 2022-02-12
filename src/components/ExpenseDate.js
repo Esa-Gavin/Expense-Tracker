8d4f0003f3ce791f5e7795da251cff11df8bab70
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-12 17:50:33 +0300

    change date to day in the expensedate js file

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -3,13 +3,13 @@ import './ExpenseDate.css';
 function ExpenseDate(props) {
     const mwezi = props.tarehe.toLocaleString('en-US', { month: 'long' });
     const mwaka = props.tarehe.toLocaleString('en-US', { day: '2-digit' });
-    const date = props.tarehe.getFullYear();
+    const day = props.tarehe.getFullYear();
 
     return (
         <div className='expense-date'>
             <div className='expense-date__month'>{mwezi}</div>
             <div className='expense-date__year'>{mwaka}</div>
-            <div className='expense-date__day'>{date}</div>
+            <div className='expense-date__day'>{day}</div>
         </div>
     )
 }
