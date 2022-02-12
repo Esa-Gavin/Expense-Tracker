ecafe54d2ec98e40db5e38da3bb6cb0714d0b5ae
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-12 16:48:33 +0300

    learn about how to pass properties from one component to another

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2022, 7, 14),
+    },
+
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2022, 2, 12)
+    },
+
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>My Expense Tracker!</h2>
-      <ExpenseItem />
+      <ExpenseItem kichwa={expenses[0].title} bei={expenses[0].amount} tarehe={expenses[0].date} />
+      <ExpenseItem kichwa={expenses[1].title} bei={expenses[1].amount} tarehe={expenses[1].date} />
+      <ExpenseItem kichwa={expenses[2].title} bei={expenses[2].amount} tarehe={expenses[2].date} />
+      <ExpenseItem kichwa={expenses[3].title} bei={expenses[3].amount} tarehe={expenses[3].date} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 2, 9);
-    const expenseTitle = 'Car Insurance';
-    const expensePrice = 800;
+function ExpenseItem(props) { // the word props can be set to any other name //
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.tarehe.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expensePrice}</div>
+                <h2>{props.kichwa}</h2>
+                <div className='expense-item__price'>${props.bei}</div>
             </div>
         </div>
     );
