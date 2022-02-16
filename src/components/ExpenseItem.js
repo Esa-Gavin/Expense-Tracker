b8696a92b0dcdf8451ac74cd536ef796c56a06ff
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-16 17:17:47 +0300

    learn about composition, specifically about children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) { // the word props can be set to any other name //
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate tarehe={props.tarehe} />
             <div className='expense-item__description'>
                 <h2>{props.kichwa}</h2>
                 <div className='expense-item__price'>${props.bei}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
