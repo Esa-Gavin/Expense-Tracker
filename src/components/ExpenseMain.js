ef913f18479e55997db9b5687fa72c74a58eb455
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-02-16 21:57:49 +0300

    add the custom element to the expense main file

--- src/components/ExpenseMain.js
+++ src/components/ExpenseMain.js
@@ -1,14 +1,15 @@
 import './ExpenseMain.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 function ExpenseMain(props) {
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem kichwa={props.items[0].title} bei={props.items[0].amount} tarehe={props.items[0].date} />
             <ExpenseItem kichwa={props.items[1].title} bei={props.items[1].amount} tarehe={props.items[1].date} />
             <ExpenseItem kichwa={props.items[2].title} bei={props.items[2].amount} tarehe={props.items[2].date} />
             <ExpenseItem kichwa={props.items[3].title} bei={props.items[3].amount} tarehe={props.items[3].date} />
-        </div>
+        </Card>
 
     )
 }
