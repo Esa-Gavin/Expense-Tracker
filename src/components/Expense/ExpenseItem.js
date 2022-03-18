67bf9622226289b335d34e99b75bf28974ee48f7
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-03-18 16:36:53 +0300

    add a button

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -10,6 +10,7 @@ function ExpenseItem(props) { // the word props can be set to any other name //
                 <h2>{props.kichwa}</h2>
                 <div className='expense-item__price'>${props.bei}</div>
             </div>
+            <buttton>Change the Title!</buttton>
         </Card>
     );
 }
