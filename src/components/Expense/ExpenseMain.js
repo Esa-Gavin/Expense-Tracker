120d618e3c01c3285d82d45ee957f7fe72db4ade
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-14 21:46:53 +0300

    rename files

--- src/components/Expense/ExpenseMain.js
+++ src/components/Expense/ExpenseMain.js
@@ -1,15 +1,21 @@
 import './ExpenseMain.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const ExpenseMain = (props) => {
     return (
-        <Card className='expenses'>
-            <ExpenseItem kichwa={props.items[0].title} bei={props.items[0].amount} tarehe={props.items[0].date} />
-            <ExpenseItem kichwa={props.items[1].title} bei={props.items[1].amount} tarehe={props.items[1].date} />
-            <ExpenseItem kichwa={props.items[2].title} bei={props.items[2].amount} tarehe={props.items[2].date} />
-            <ExpenseItem kichwa={props.items[3].title} bei={props.items[3].amount} tarehe={props.items[3].date} />
-        </Card>
+        <div>
+            <ExpensesFilter />
+            <Card className='expenses'>
+                <ExpenseItem kichwa={props.items[0].title} bei={props.items[0].amount} tarehe={props.items[0].date} />
+                <ExpenseItem kichwa={props.items[1].title} bei={props.items[1].amount} tarehe={props.items[1].date} />
+                <ExpenseItem kichwa={props.items[2].title} bei={props.items[2].amount} tarehe={props.items[2].date} />
+                <ExpenseItem kichwa={props.items[3].title} bei={props.items[3].amount} tarehe={props.items[3].date} />
+            </Card>
+
+        </div>
+        
 
     )
 }
