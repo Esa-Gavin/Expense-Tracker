12edeebd58e8552d663d292e45446bbc2d9def3c
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-14 16:56:04 +0300

    add comments about two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,7 +30,11 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
-        console.log(expenseData)
+        console.log(expenseData);
+
+        setEnteredTitle(''); // we do this so as to return the value back to an empty string, thus two way binding has been achieved //
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
     return (
@@ -39,15 +43,15 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
