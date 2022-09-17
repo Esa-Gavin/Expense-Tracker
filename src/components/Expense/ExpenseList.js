c3af845de6b30b013a57a09b28c7ff86a3c61ff3
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-17 17:07:20 +0300

    add comments

--- src/components/Expense/ExpenseList.js
+++ src/components/Expense/ExpenseList.js
@@ -3,13 +3,13 @@ import React from 'react';
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
 
-const ExpenseList = () => {
+const ExpenseList = (props) => {
     
     // conditional statement //
     let expensesContent = <p>No expenses found here!</p>
-
-    if (filteredMwaka.length > 0) {
-        expensesContent = filteredMwaka.map(expense => <ExpenseItem key={expense.id} kichwa={expense.title} bei={expense.amount} tarehe={expense.date} />)
+    // the name items is up to you, it can be named anything //
+    if (props.items.length > 0) {
+        expensesContent = props.items.map(expense => <ExpenseItem key={expense.id} kichwa={expense.title} bei={expense.amount} tarehe={expense.date} />)
     }
     // conditional statement //
 
