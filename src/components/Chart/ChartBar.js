0f7e1b1c59111c0453cfde036fec780b49e9dc95
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-17 19:44:24 +0300

    convert the value into percentage and also learn how to use inline styling in react

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 import './ChatBar.css';
 
-const ChatBar = () => {
-    <div className='chart-bar'>
-        <div className='chart-bar__inner'>
-            <div className='chart-bar__fill'></div>
+const ChatBar = (props) => {
+
+    let barFillHeight = '0%';
+
+    //math.round is for rounding to the next integer //
+    if (props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%'
+    }
+
+    return (
+        <div className='chart-bar'>
+            <div className='chart-bar__inner'>
+                <div className='chart-bar__fill' style={{height: barFillHeight}}></div>
+            </div>
             <div className='chart-bar__label'></div>
         </div>
-    </div>
+
+    )
+    
 }
 
 export default ChatBar;
