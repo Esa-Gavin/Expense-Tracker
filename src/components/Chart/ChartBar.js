42787051012473a5af9db8eff2b597c79015a42a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-17 19:27:06 +0300

    add the various divs in the chartbarjs

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,12 @@ import React from 'react';
 import './ChatBar.css';
 
 const ChatBar = () => {
-
+    <div className='chart-bar'>
+        <div className='chart-bar__inner'>
+            <div className='chart-bar__fill'></div>
+            <div className='chart-bar__label'></div>
+        </div>
+    </div>
 }
 
 export default ChatBar;
