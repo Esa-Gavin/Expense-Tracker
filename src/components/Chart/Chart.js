3c5eb388d7b897c905f26bc6888bf7f522cf4994
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-09-17 19:22:49 +0300

    add the chartbar component in the chartjs file

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import ChatBar from './ChartBar';
 import './Chart.css';
 
-const Chart = () => {
+const Chart = (props) => {
+    return (
+        <div className='chart'>
+            {props.dataPoints.map(dataPoint =>
+                <ChatBar
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={null}
+                    label={dataPoint.label} />)}
+        </div>
+    )
 
 }
 
